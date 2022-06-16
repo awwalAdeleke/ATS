9effd75d0572f49f390c99caf22a3cdb36da23e2
awwalAdeleke <aadeleke@afexnigeria.com>
2022-06-16 11:04:40 +0100

    Learning

---
+++ class_assessments/week-2/36-pos_arg_average.py
@@ -0,0 +1,9 @@
+def average(no1, no2, no3, no4, no5):
+    average = (no1 + no2 + no3 + no4 + no5)/5
+    print(average)
+
+average(no1 = 1, no2 = 5, no3 = 10, no4 = 15, no5 = 25)
+
+average(2, 5, 6, 9, 20)
+
+
---
+++ class_assessments/week-2/37-data-type.py
@@ -0,0 +1,8 @@
+def data_type(num:int, letter:str, list_objects:list, tuple_objects:tuple, set_objects:set):
+    print(type(num))
+    print(type(letter))
+    print(type(list_objects))
+    print(type(tuple_objects))
+    print(type(set_objects))
+    
+data_type(4, "string", [1,2,3], (3,4,5), {"set", 2})
